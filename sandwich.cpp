a5dbb8547c5b1bebd87764d3928dbda9c990a7ef
1urch <charleshreed10@gmail.com>
2023-01-26 00:44:42 -0500

    Create sandwich.cpp

---
+++ sandwich.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+bool morning = true;
+
+std::string make_sandwich(){
+std::string sandwich = "";
+sandwich += "bread\n";
+  if (morning == true){
+  sandwich += "egg\n";}
+  sandwich += "cheese\n";
+  sandwich += "bread\n";
+  return sandwich;
+}
+
+int main {
+std::cout << "Your sandwich is now:\n" << make_sandwich();
+}
+
