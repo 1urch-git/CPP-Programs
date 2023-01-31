8a76815402255b310f23036521fae9abea799935
1urch <charleshreed10@gmail.com>
2023-01-31 01:04:38 -0500

    Update palindrome.cpp

--- palindrome.cpp
+++ palindrome.cpp
@@ -3,8 +3,20 @@
 // Define is_palindrome() here:
 bool is_palindrome(std::string text){
 
-}
+//initialize iterators, and subtract one from counter due to indexing
+int i=0;
+int counter = text.size()-1;
 
+//while loop to cycle thorugh letters
+while(i<text.size()){
+ if(text[i]==text[counter]){
+  i++;
+  counter--;
+ }
+ else{return false;} //break loop and return false once it doesn't meet criteria
+ return true;
+ }
+}
 
 int main() {
   
@@ -13,3 +25,5 @@ int main() {
   std::cout << is_palindrome("lovelace") << "\n";
   
 }
+
+ //output will be 1,1,0
