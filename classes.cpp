1a3b1f29dc7f6b77a913052cecd3804268b3ec47
1urch <charleshreed10@gmail.com>
2023-02-09 23:59:00 -0500

    Update classes.cpp

--- classes.cpp
+++ classes.cpp
@@ -1,3 +1,9 @@
 
 
 //placeholder for Classes and Objects lessons
+//Class is a blueprint with attributes and functions describing data, but creates no objects itself when defined/created
+//Objects are instances of a class, created using Classes as the blueprint
+
+class Song [
+  
+  ];
