0330af4137725fcf95186c23fb1810edb4326c51
1urch <charleshreed10@gmail.com>
2023-02-11 23:58:38 -0500

    Create song.hpp

---
+++ song.hpp
@@ -0,0 +1,17 @@
+#include <string>
+
+//for use with song.cpp and music.cpp
+
+class Song {
+  
+  std::string title;
+  std::string artist;
+
+public:
+  void add_title(std::string new_title);
+  std::string get_title();
+  void add_artist(std::string new_artist);
+  std::string get_artist();
+  
+};
+
